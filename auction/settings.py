9a0f00c13d9ca3a7aabfcc044d230925c5f735f6
CrazySneeze <matthew.k00@outlook.com>
2022-12-22 02:09:55 +0000

    fix 2

--- auction/settings.py
+++ auction/settings.py
@@ -9,6 +9,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    '*',
 ]
 
 
@@ -23,7 +24,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:5173', 'http://localhost:5173',]
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:5173', 'http://localhost:5173', '*',]
 
 INSTALLED_APPS = [
     "django_crontab",
